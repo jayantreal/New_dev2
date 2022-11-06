5500f4a3cd30586e1f0ef62bd2b09e827496fc7d
Jayant <jayantkumar9181835@gmail.com>
2022-11-07 03:11:11 +0530

    exported_file1

---
+++ Lecture_2file/until.js
@@ -0,0 +1,9 @@
+let extensions = {
+    "Images":[".png",".jpg",".jpeg",".gif"],
+    "Audio":[".mp4",".mp3"],
+    "Documents":[".pdf",".txt",".doc"],
+    "Compressed":[".zip"],
+    "Videos":[".mkv"]
+}
+
+module.exports=extensions; 
