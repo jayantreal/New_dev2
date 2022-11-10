0d3f14dee5734d1b7801a6d47a21a2d0dc5e103b
Jayant <jayantkumar9181835@gmail.com>
2022-11-10 18:21:53 +0530

    complete_functon

--- Lecture_3/Facts.js
+++ Lecture_3/Facts.js
@@ -1,5 +1,4 @@
 let content = process.argv.slice(2);
-
 let fs= require("fs");
 
 let flags=[];
@@ -18,7 +17,85 @@ for(let i=0;i<content.length;i++){
 let fileKadata="";
 for(let i=0;i<files.length;i++){
     fileKadata+=fs.readFileSync(files[i]);
-    fileKadata+=" ";
+    fileKadata+="\r\n";
 }
 
-console.log(fileKadata);
+//console.log(fileKadata);
+
+if(!flags.length){
+    console.log(fileKadata);
+    return;
+}
+
+let data =fileKadata.split("\r\n");
+
+let removeSpaces =[];
+
+if(flags.includes("-s")){
+removeLargeSpace(data);
+}
+
+if(flags.includes("-n") && flags.includes("-b")){
+    if(flags.indexOf("-n") < flags.indexOf("-b")){
+        if(flags.includes("-s")){
+            addLineNumberToallLines(removeSpaces);
+        }else{
+            addLineNumberToallLines(data);
+        }
+    }
+}else if(flags.includes("-n")){
+    if(flags.includes("-s")){
+        addLineNumberToallLines(removeSpaces);
+    }else{
+        addLineNumberToallLines(data);
+
+    }
+
+}
+else if(flags.includes("-b")){
+    if(flags.includes("-s")){
+        addLineNumberToNonEmptyLine(removeLargeSpace);
+    }else{
+        addLineNumberToallLines(data);
+    }
+
+}
+
+
+
+
+
+
+function addLineNumberToallLines(data){
+    for(let i=1 ; i<data.length+1;i++){
+        data[i-1]=`${i}.${data[i-1]}`;
+    }
+    let addedLineNumber = data.join("\n");
+    console.log(addedLineNumber);
+}
+
+function addLineNumberToNonEmptyLine(data){
+    let count=1;
+
+    for(let i=0;i<data.length;i++){
+        if(data[i]!=''){
+            data[i]=`${count}.${data[i]}`;
+            count++;
+        }
+    }
+    let addLineNumber = data.join("\n");
+    console.log(addLineNumber);
+}
+
+function removeLargeSpace(data){
+    let emptyPushed =false;
+    for(let i=0;i<data.length;i++){
+        if(data[i]== '' && !emptyPushed){
+            removeSpaces.push(data[i]);
+            emptyPushed=true;
+        }
+        else if(data[i]!=''){
+            removeSpaces.push(data[i]);
+        }
+    }
+}
